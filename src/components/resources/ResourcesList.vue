71eb23b12db302f8f4e2abe6939d599f20d4e4c8
jcastro97 <jcastro@crnova.com>
2022-08-02 20:06:13 -0600

    list styles

--- src/components/resources/ResourcesList.vue
+++ src/components/resources/ResourcesList.vue
@@ -17,3 +17,13 @@ export default {
   components: { Resource },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
