51d1d6bc9550b2db9548772d50376d8ed41e723f
jcastro97 <jcastro@crnova.com>
2022-08-02 19:24:55 -0600

    set up App data function return object

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Search engine for finding the best information',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
